6ec122357bb596dc29e30203c7b1032f2312ef36
venkateshb24 <venkatesh.b2024aiml@sece.ac.in>
2025-12-18 12:09:13 +0530

    Made changes

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -15,7 +15,7 @@ export default function Register({ onRegister }) {
       return;
     }
 
-    const res = await fetch("http://localhost:5000/api/auth/register", {
+    const res = await fetch("http://51.21.132.235:5000/api/auth/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4000,
+    host: true   // VERY IMPORTANT for EC2 / external access
+  }
 })
+
